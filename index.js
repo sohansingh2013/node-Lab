8f4f7fede21c8ef802b64882e3ac6b1c7f42132d
sohansingh2013 <sohansingh2013@outlook.com>
2023-06-08 05:03:38 +0530

    2 nd

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 console.log("Welcode to node JS to run in terminal- node .\index.js ");
-var a=20 ;
-var b=30;
-var c= 40;
+var a = 20;
+var b = 30;
+var c = 40;
 // console.warn(a+b+c);
 
 var x = '70';
 // === to cheack the value type also if else condation
-if(x===70)
-{
-    console.log(" not Match");
+if (x === 70) {
+  console.log(" not Match");
 }
-var x= 20;
-if(x==20)
-{
+var x = 20;
+if (x == 20) {
   //  console.log("Match");
 }
 // for loop exmple
-for(i=0;i<=10;i++)
-{
+for (i = 0; i <= 10; i++) {
   //  console.log(i);
 }
 // arry define and filter uses 
-const arr =[2,4,7,1,3,8,9,3];
+const arr = [2, 4, 7, 1, 3, 8, 9, 3];
 //console.log(arr);
 //console.log(arr[2]);
 
-arr.filter((item)=>{
+arr.filter((item) => {
   //  console.log(item);
-  
+
 })
 
-let result = arr.filter((item)=>{
+let result = arr.filter((item) => {
 
-   // return item ===3
-   // return item >4
-    return item >=4
-  })
- // console.log(result);
+  // return item ===3
+  // return item >4
+  return item >= 4
+})
+// console.log(result);
 
 // imports the class or prog or function in node js
 const app = require('./app')
@@ -50,7 +47,7 @@ const app = require('./app')
 
 const fs1 = require('fs');
 console.log("Consloe is globle function no need to import");
-fs1.writeFileSync("hello.txt","This message need to write in file - code step by step  file syste is need to import that means non globle");
+fs1.writeFileSync("hello.txt", "This message need to write in file - code step by step  file syste is need to import that means non globle");
 console.log("->>", __dirname);
 console.log("->>>", __filename);
 
@@ -64,19 +61,38 @@ const http = require('http');
 
 // Tis is use arrow function .....
 //http.createServer((req , resp) => {
-    //resp.write("<h1>Hello this is sohan singh....</h1>");
-     //resp.end();
- // }).listen(4500);
+//resp.write("<h1>Hello this is sohan singh....</h1>");
+//resp.end();
+// }).listen(4500);
 
 // anoter way to pass the req and response  without arrow function 
 
-function dataControl (req , resp)
-{
-    resp.write("<h1>Hello , This is Sohan Kumar Singh....</h1>");
-    resp.end();
+function dataControl(req, resp) {
+  resp.write("<h1>Hello , This is Sohan Kumar Singh....</h1>");
+  resp.end();
 }
 
- // http.createServer(dataControl).listen(4500);
+// http.createServer(dataControl).listen(4500);
 
 // all About Package .json
 // git configration done
+// git command
+// open vs code on cmd go to folder locatin and type - code . it should open vscode if install
+//git init
+// git status
+//git add index.js
+//git status
+//git commit -m "initial commit"
+//git log
+// if you want to open git online type - https://vscode.dev/github/sohansingh2013/node-tut
+
+//npm init
+//npm i colors
+//npm i simple-node-logger
+
+// if deleted the node-modeule you can use npm install
+
+var colors = require('colors');
+console.log('hello'.green);
+
+console.log("Package.json".red);
